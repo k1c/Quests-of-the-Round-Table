473b4feb8042a4602d690463f8b795cb20992937
christopherblackman <christopherblackman@dhcp-158-208.cu-wireless-students.carleton.ca>
2018-01-30 11:34:44 -0500

    updated for multiple Observers

--- build/my-app/src/main/java/com/mycompany/app/GameModel.java
+++ build/my-app/src/main/java/com/mycompany/app/GameModel.java
@@ -1,13 +1,32 @@
 
 package com.mycompany.app;
 
+import com.mycompany.app.Observer;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
 public class GameModel{
 
-	private Observer observer = null;
+	private ArrayList<Observer> observers;
 
 	public GameModel(){
+		observers = new ArrayList<Observer>();
 	}
+
+	public void registerObserver(Observer o){
+		this.observer = o;
+	}
+
+	public void deregisterObserver(){
+		this.observer = null;
+	}
+
 	public void notify(){
-		observer.update()
+		for(Iterator<Observer> i = observers.Iterator(); .hasNext();){
+			Observer item = item.next();
+			item.notify()
+		}
 	}
 }
