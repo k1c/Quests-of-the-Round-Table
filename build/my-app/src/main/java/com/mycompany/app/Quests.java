9cdd9a4ec9b980bee5fb7e84a36ffbbef52f57d8
Akhil Dalal <akhil.d@hotmail.com>
2018-02-04 02:10:51 -0500

    Local test (#75)
    * fixed image path and refactored resources folder
    
    * fixed image path and refactored resources folder

--- build/my-app/src/main/java/com/mycompany/app/Quests.java
+++ build/my-app/src/main/java/com/mycompany/app/Quests.java
@@ -1,6 +1,10 @@
 package com.mycompany.app;
 
+import com.mycompany.app.view.AdventureDeckView;
+import com.mycompany.app.view.StoryDeckView;
+
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
@@ -18,24 +22,26 @@ public class Quests extends Application {
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
-    private static final int MARGING_OUTER = 5;
-    private static final String TITLE = "Quests of the Roundtable";
+    private static final int MARGIN_OUTER = 5;
+    private static final String TITLE = "Quests of the Round Table";
 
-    //private AdventureDeckView adventureDeckView = new AdventureDeckView();
+    private AdventureDeckView adventureDeckView = new AdventureDeckView();
+    private StoryDeckView storyDeckView = new StoryDeckView();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         primaryStage.setTitle(TITLE);
 
         GridPane root = new GridPane();
+        root.setAlignment(Pos.TOP_RIGHT);
         root.setStyle("-fx-background-color: d3d3d3;");
-        root.setHgap(MARGING_OUTER);
-        root.setVgap(MARGING_OUTER);
-        root.setPadding(new Insets(MARGING_OUTER));
+        root.setHgap(MARGIN_OUTER);
+        root.setVgap(MARGIN_OUTER);
+        root.setPadding(new Insets(MARGIN_OUTER));
 
-        //root.add(adventureDeckView,0,0);
-
-        root.setGridLinesVisible(false); //used for debugging
+        root.add(adventureDeckView,0,0);
+        root.add(storyDeckView, 1, 0);
+        root.setGridLinesVisible(true); //used for debugging
 
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
