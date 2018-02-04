39995e5cff62d8761c8056adb9c9106e4a843d45
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-04 18:41:02 -0500

    fixed card loader

--- build/my-app/src/main/java/com/mycompany/app/model/CardLoader.java
+++ build/my-app/src/main/java/com/mycompany/app/model/CardLoader.java
@@ -1,15 +1,16 @@
 package com.mycompany.app.model;
 
-import static java.util.Arrays.ArrayList;
 
-import com.mycompany.app.model.*;
+import com.mycompany.app.model.AdventureCard;
+import com.mycompany.app.model.AdventureCardFactory;
 import java.util.*;
 
 
 public class CardLoader{
 	public ArrayList<AdventureCard> loadAdventureCards (){
-		ArrayList<AdventureCard> = new ArrayList<AdventureCard>();
+		ArrayList<AdventureCard> cards = new ArrayList<AdventureCard>();
 		AdventureCardFactory F = new AdventureCardFactory();
+		return  cards;
 	}
 
 }
