2db027173812d8d1a7b1a01652268888ead6deee
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-06 21:27:24 -0500

    added init game and constatans

--- build/my-app/src/main/java/com/mycompany/app/model/GameBoard.java
+++ build/my-app/src/main/java/com/mycompany/app/model/GameBoard.java
@@ -1,30 +1,49 @@
 package com.mycompany.app.model;
 
 import java.util.ArrayList;
+import java.lang.Math;
 
 import com.mycompany.app.model.AdventureCard;
 import com.mycompany.app.model.GameBoard;
+import com.mycompany.app.model.Player;
 import com.mycompany.app.model.StoryCard;
 
 public class GameBoard{
+	private final int MIN_PLAYERS = 2;
+	private final int MAX_PLAYERS = 4;
+
 	private static GameBoard board = null;
 
 	protected ArrayList<AdventureCard> 	adventureDeck;
 	protected ArrayList<AdventureCard> 	adventureDeckDiscard;
 	protected ArrayList<StoryCard> 		storyDeck;
 	protected ArrayList<StoryCard> 		storyDeckDiscard;
+	protected ArrayList<Player>		players;
 
 	private GameBoard(){
-		this.adventureDeck 	  = new ArrayList<AdventureCard>();	
-		this.adventureDeckDiscard = new ArrayList<AdventureCard>();	
-		this.storyDeck		  = new ArrayList<StoryCard>();	
-		this.storyDeckDiscard 	  = new ArrayList<StoryCard>();	
+			
 	}
 
 	public static GameBoard getInstance(){
 		if(board == null)
 			board = new GameBoard();
 		return board;
+	}
+
+	public void init_Game(){
+		init_Game(MAX_PLAYERS);
+	}
+	public void init_Game(int num){
+		num = Math.min(Math.max(MAX_PLAYERS,num),MIN_PLAYERS);
+
+		this.adventureDeck 	  = new ArrayList<AdventureCard>();	
+		this.adventureDeckDiscard = new ArrayList<AdventureCard>();	
+		this.storyDeck		  = new ArrayList<StoryCard>();	
+		this.storyDeckDiscard 	  = new ArrayList<StoryCard>();
+		this.players 		  = new ArrayList<Player>();
+
+		for(int i = 0; i < num; i++)
+			this.players.add(new Player());
 	}
 
 	
