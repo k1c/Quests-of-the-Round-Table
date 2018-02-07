9125effcb4a7f32d23818d34ad7217f936dd6d76
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-06 23:01:05 -0500

    - removed abstract class - enfourced instantiation with protected contructors - added an instance method to card for a client server archetechture

--- build/my-app/src/main/java/com/mycompany/app/model/Card.java
+++ build/my-app/src/main/java/com/mycompany/app/model/Card.java
@@ -1,16 +1,27 @@
 package com.mycompany.app.model;
 
-public abstract class Card{
+
+import com.mycompany.app.model.Card;
+
+public class Card {
 	public static enum Types {FOE,ALLY,WEAPON,AMOUR,TEST, EVENT, QUEST, TOURNAMENT};
 	public final Types type;
 	public final int id;
 	public final String res;
 	public final String name;
 
-	public Card(int id,String res,String name, Types type){
+	protected Card(int id,String res,String name, Types type){
 		this.name = name;
 		this.res = res;
 		this.id = id;
 		this.type = type;
+	}
+
+	public Card(Card original){
+		this(original.id, original.res, original.name, original.type);
+	}
+
+	public Card instance(){
+		return new Card(this);
 	}
 }
