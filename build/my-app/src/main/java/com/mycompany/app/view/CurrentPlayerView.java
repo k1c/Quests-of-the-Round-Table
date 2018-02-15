287335a752e74258c2e5165fc497cad246969727
Akhil Dalal <akhil.d@hotmail.com>
2018-02-14 23:36:21 -0500

    cleaned it up and added modularity

--- build/my-app/src/main/java/com/mycompany/app/view/CurrentPlayerView.java
+++ build/my-app/src/main/java/com/mycompany/app/view/CurrentPlayerView.java
@@ -1,3 +1,10 @@
+/**
+ * Author: Akhil Dalal
+ * As part of TEAM 4
+ *
+ * CurrentPlayerView - Renders and sets up view for current player
+ *                   - TODO: Add eventhandler for playing the cards
+ */
 package com.mycompany.app.view;
 
 import com.mycompany.app.model.*;
@@ -16,119 +23,162 @@ import javafx.scene.input.MouseEvent;
 
 /**
  * Potential issue:
- *  1) Currently passing in the game model object
- *      - Any way to avoid this or make it better?
+ * 1) Currently passing in the game model object
+ * - Any way to avoid this or make it better?
  */
-public class CurrentPlayerView extends StackPane implements GameObserver{
+public class CurrentPlayerView extends StackPane implements GameObserver {
 
     private GameModel model;
     private ViewGameBoard currentGameState;
+
     private static final int PADDING = 5;
     private static final int X_OFFSET = -75;
-    private double currX;
-    public CurrentPlayerView(GameModel gameModel){
+
+    public CurrentPlayerView(GameModel gameModel) {
         this.model = gameModel;
+        this.model.registerObserver(this);
         this.currentGameState = model.getGameBoard();
+
         setPadding(new Insets(PADDING));
-        setAlignment(Pos.TOP_CENTER);
-        gameModel.registerObserver(this);
+        setAlignment(Pos.BOTTOM_CENTER);
+
         buildLayout();
     }
 
     private void buildLayout() {
         getChildren().clear();
 
+        // rank
+        buildRank();
+
+        // in hand
+        buildHand();
+
+        // in play
+        buildInPlay();
+    }
+
+    private void buildRank() {
+
+    }
+
+    private void buildHand(){
         int offset = 0;
+
         // get current player
         GenericPlayer current = currentGameState.players.get(0);
-        // get current player hand and display
-        ArrayList<Card> hand = current.hand;
-        for(final Card card : hand) {
-            // FOE,ALLY,WEAPON,AMOUR,TEST
-            final Image im = new Image(card.res);
-            final ImageView image = new ImageView(im);
-            image.setId(card.id+"");
-            image.setTranslateX(X_OFFSET*offset);
-            switch (card.type) {
-                case FOE:
-                    image.setStyle("-fx-effect: innershadow(gaussian, white, 10, 0.0, 0, 0);");
-                    break;
-                case ALLY:
-                    image.setStyle("-fx-effect: innershadow(gaussian, deepskyblue, 10, 0.0, 0, 0);");
-                    break;
-                case TEST:
-                    image.setStyle("-fx-effect: innershadow(gaussian, forestgreen, 10, 0.0, 0, 0);");
-                    break;
-                case AMOUR:
-                    image.setStyle("-fx-effect: innershadow(gaussian, yellow, 10, 0.0, 0, 0);");
-                    break;
-                case WEAPON:
-                    image.setStyle("-fx-effect: innershadow(gaussian, red, 10, 0.0, 0, 0);");
-                    break;
-            }
 
+        // get current player hand
+        ArrayList<Card> hand = current.hand;
+
+        // For each card, add it to stackpane and attach eventhandlers
+        for (Card card : hand) {
+            final ImageView image = new ImageView(new Image(card.res));
+
+            // Scale image
+            image.setFitHeight(200);
+            image.setFitWidth(146);
+
+            // Move to left for 'stack' effect
+            image.setTranslateX(X_OFFSET * offset);
+
+            // Add 'correct' position of the card in the stackpane
+            // correct means visual index instead of calculated z-index which changes
+            image.setId(offset + "");
+
+            // Add border with same color as card
+            image.getProperties().put("color", getColor(card));
+
+            image.setStyle((String) image.getProperties().get("color"));
+
+            // Offset/Position for next card
             offset++;
+
+            // Add to StackPane
             getChildren().add(image);
 
-            image.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    image.setStyle("-fx-effect: dropshadow(gaussian, #ff7c14, 10, 0.7, 0, 0);");
+            // Hover on card -
+            // Brings it to front, rearranges other cards for easy access.
+            image.addEventHandler(MouseEvent.MOUSE_ENTERED, focusCard(image));
 
-                    ObservableList<Node> children = getChildren();
-                    Node[] temp = new Node[children.size()];
-
-                    for  (int i = 0; i < children.size(); i++) {
-                        temp[i] = null;
-                    }
-
-
-                    for (Node child : children) {
-                        int aIndex = (int) Math.abs(child.getTranslateX() / X_OFFSET);
-                        temp[aIndex] = child;
-                    }
-
-                    List<Node> sortedChildren = new ArrayList<Node>(Arrays.asList(temp));
-                    int index = sortedChildren.indexOf(image);
-
-                    for (int i = 0; i < index; i++) {
-                        sortedChildren.get(i).toFront();
-                    }
-
-                    for (int i = sortedChildren.size() - 1; i > index; i--) {
-                        sortedChildren.get(i).toFront();
-                    }
-
-                   sortedChildren.get(index).toFront();
-
-                }
-            });
-
+            // Reset border color when mouse is no longer hovering on card
             image.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
-                @Override
                 public void handle(MouseEvent event) {
-                    switch (card.type) {
-                        case FOE:
-                            image.setStyle("-fx-effect: innershadow(gaussian, white, 10, 0.0, 0, 0);");
-                            break;
-                        case ALLY:
-                            image.setStyle("-fx-effect: innershadow(gaussian, deepskyblue, 10, 0.0, 0, 0);");
-                            break;
-                        case TEST:
-                            image.setStyle("-fx-effect: innershadow(gaussian, forestgreen, 10, 0.0, 0, 0);");
-                            break;
-                        case AMOUR:
-                            image.setStyle("-fx-effect: innershadow(gaussian, yellow, 10, 0.0, 0, 0);");
-                            break;
-                        case WEAPON:
-                            image.setStyle("-fx-effect: innershadow(gaussian, red, 10, 0.0, 0, 0);");
-                            break;
-                    }
+                    image.setStyle((String) image.getProperties().get("color"));
                 }
             });
-        } // end for
+        }
+    }
 
-        // get current player in-play and display
+    private void buildInPlay() {
+
+    }
+
+    private EventHandler<MouseEvent> focusCard(final ImageView image) {
+        return new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+
+                // Selection color
+                image.setStyle("-fx-effect: dropshadow(gaussian, #ff7c14, 5, 1, 0, 0)");
+
+                // Get all current cards on display
+                List<Node> children = new ArrayList<Node>(getChildren());
+
+                // Sort by actual index instead of z-index
+                Collections.sort(children, new Comparator<Node>() {
+                    public int compare(Node o1, Node o2) {
+                        return Integer.parseInt(o1.getId()) - Integer.parseInt(o2.getId());
+                    }
+                });
+
+                // Get position of card user is hovering over
+                int index = children.indexOf(image);
+
+                // Start to re-arrange cards from both ends till they meet
+                int right = 0;
+                int left = children.size() - 1;
+
+                while (right != left) {
+                    if (!children.get(right).equals(image)) {
+                        children.get(right).toFront();
+                        right++;
+                    }
+
+                    if (!children.get(left).equals(image)) {
+                        children.get(left).toFront();
+                        left--;
+                    }
+                }
+
+                // Bring target card in front
+                children.get(index).toFront();
+            }
+        };
+    }
+    private String getColor(Card card) {
+        String css = "-fx-effect: innershadow(gaussian, ";
+
+        switch (card.type) {
+            case FOE:
+                css += "white, ";
+                break;
+            case ALLY:
+                css += "deepskyblue, ";
+                break;
+            case TEST:
+                css += "forestgreen, ";
+                break;
+            case AMOUR:
+                css += "yellow, ";
+                break;
+            case WEAPON:
+                css += "red, ";
+                break;
+        }
+
+        css += "10, 0.7, 0, 0);";
+
+        return css;
     }
 
     public void update() {
