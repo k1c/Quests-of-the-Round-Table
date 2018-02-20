22fa40d9f38684b64cbdc9e18d6e9a5c53195f40
d4sh <akhil.d@hotmail.com>
2018-02-19 19:48:06 -0500

    setup GameView and GameController

--- build/my-app/src/main/java/com/mycompany/app/Quests.java
+++ build/my-app/src/main/java/com/mycompany/app/Quests.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import com.mycompany.app.controller.GameController;
+import com.mycompany.app.model.GameObserver;
 import com.mycompany.app.view.*;
 import com.mycompany.app.model.GameModel;
 import javafx.application.Application;
@@ -11,23 +13,21 @@ import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 
 /**
- * Quests class brings together major UI compoements and launches the application.
+ * Quests class brings together major UI components and launches the application.
  *
  * to run:
  *   mvn package
  *   java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.Quests
  */
 
-public class Quests extends Application {
+public class Quests extends Application{
 
     private static final int WIDTH = 1920;
     private static final int HEIGHT = 1080;
-    private static final int MARGIN_OUTER = 5;
     private static final String TITLE = "Quests of the Round Table";
 
     private GameModel model = new GameModel();
-    private CurrentPlayerView currentPlayerView = new CurrentPlayerView(model);
-    private WaitingPlayersView waitingPlayersView = new WaitingPlayersView(model);
+    private GameView view = new GameView(model);
 
     @Override
     public void start(Stage primaryStage) {
@@ -35,29 +35,14 @@ public class Quests extends Application {
 
         BorderPane root = new BorderPane();
 
-        GridPane currentPlayer = new GridPane();
+        root.setCenter(view);
 
-        currentPlayer.setPadding(new Insets(MARGIN_OUTER));
-        currentPlayer.setAlignment(Pos.BOTTOM_RIGHT);
+        Scene start = new Scene(root, WIDTH, HEIGHT);
 
-        currentPlayer.add(currentPlayerView, 0, 0);
-
-        root.setBottom(currentPlayer);
-
-        GridPane waitingPlayers = new GridPane();
-
-        waitingPlayers.setPadding(new Insets(MARGIN_OUTER));
-        waitingPlayers.setAlignment(Pos.CENTER_RIGHT);
-
-        waitingPlayers.add(waitingPlayersView, 0, 0);
-
-        root.setRight(waitingPlayers);
-
-        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setScene(start);
         primaryStage.setMaximized(true);
         primaryStage.setResizable(true);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
--- build/my-app/src/main/java/com/mycompany/app/controller/GameController.java
+++ build/my-app/src/main/java/com/mycompany/app/controller/GameController.java
@@ -1,24 +1,65 @@
 package com.mycompany.app.controller;
 
 import com.mycompany.app.model.GameModel;
-import com.mycompany.app.model.GameObserver;
+import com.mycompany.app.view.CurrentPlayerView;
 import com.mycompany.app.view.GameView;
+import com.mycompany.app.view.WaitingPlayersView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class GameController implements GameObserver{
+public class GameController{
+
+	private static final int MARGIN_OUTER = 5;
+	private CurrentPlayerView currentPlayerView;
+	private WaitingPlayersView waitingPlayersView;
 
 	private final GameModel model;
 	private final GameView view;
 
-	public GameController(GameModel model){
+	public GameController(GameModel model, GameView view){
+		this.view = view;
 		this.model = model;
-		this.model.registerObserver(this);
-		this.view = new GameView(this);
 	}
 
-	public void update(){
+	public void startGame(Button btn) {
+        /*
+        if (model.startGame(userOptions) == true) this.changeScene
+        else view.showError()
+         */
+		Stage primaryStage = (Stage) btn.getScene().getWindow();
 
+		currentPlayerView = new CurrentPlayerView(model);
+		waitingPlayersView = new WaitingPlayersView(model);
+
+		BorderPane root = new BorderPane();
+
+		GridPane currentPlayer = new GridPane();
+
+		currentPlayer.setPadding(new Insets(MARGIN_OUTER));
+		currentPlayer.setAlignment(Pos.BOTTOM_RIGHT);
+
+		currentPlayer.add(currentPlayerView, 0, 0);
+
+		root.setBottom(currentPlayer);
+
+		GridPane waitingPlayers = new GridPane();
+
+		waitingPlayers.setPadding(new Insets(MARGIN_OUTER));
+		waitingPlayers.setAlignment(Pos.CENTER_RIGHT);
+
+		waitingPlayers.add(waitingPlayersView, 0, 0);
+
+		root.setRight(waitingPlayers);
+
+		Scene gameScene = new Scene(root, btn.getScene().getWidth(), btn.getScene().getHeight());
+
+		primaryStage.setScene(gameScene);
 	}
-
 }
 
 
