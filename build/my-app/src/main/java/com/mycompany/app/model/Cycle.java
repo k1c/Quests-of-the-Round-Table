b529a7a0b051c4ded0a1074ac73f5c224feee324
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-19 23:26:04 -0500

    added a new data structure for cycling turns

---
+++ build/my-app/src/main/java/com/mycompany/app/model/Cycle.java
@@ -0,0 +1,41 @@
+package com.mycompany.app.model;
+import java.util.*;
+
+public class Cycle<T>{
+    protected List<T> list;
+    protected int currentIndex;
+    
+    public Cycle(List<T> list,int index){
+        this.list = new ArrayList<T>(list);
+        currentIndex = index;
+    }
+    
+    protected int getIndex(){
+        currentIndex = currentIndex % list.size();  
+        return currentIndex;
+    }
+    protected int nextIndex(){
+        currentIndex++;
+        return getIndex();
+    }
+    
+    public List<T> items(){
+        return new ArrayList<T>(list);
+    }
+    
+    public T removeCurrent(){
+        T temp = list.remove(currentIndex);
+        getIndex();
+        return temp;
+    }
+    
+    public T current(){
+        return list.get(currentIndex);
+    }
+    
+    public T next(){
+        nextIndex();
+        return list.get(currentIndex);
+    }
+    
+}
--- build/my-app/src/main/java/com/mycompany/app/model/GameModel.java
+++ build/my-app/src/main/java/com/mycompany/app/model/GameModel.java
@@ -225,7 +225,6 @@ public class GameModel{
 
 	public void applyEventLogic(){
 		if(this.state != GameStates.EVENT_LOGIC)
-
 			return;
 
 		/*
