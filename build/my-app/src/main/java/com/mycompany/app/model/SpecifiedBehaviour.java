d24e689441a646cf0caa2c997f7a16e3cde0ddee
AlexaDeGrandmont <alexa.degrandmont@gmail.com>
2018-02-21 13:01:48 -0500

    added getter for maxShields in Rank

--- build/my-app/src/main/java/com/mycompany/app/model/SpecifiedBehaviour.java
+++ build/my-app/src/main/java/com/mycompany/app/model/SpecifiedBehaviour.java
@@ -22,15 +22,19 @@ public class SpecifiedBehaviour extends DefaultBehaviour{
 
     }
 
-    public int getBP(){
+    public int getBP(GameBoard board){
+
+
         return this.BP;
     }
 
-    public int getBids(){
+    public int getBids(GameBoard board){
+
+
         return this.bids;
     }
 
-    public Boolean isFreeBid(){
+    public Boolean isFreeBid(GameBoard board){
 	return this.freeBid;
     }
 
