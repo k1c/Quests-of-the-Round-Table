f3ec0c709b2ea837c6d1512c0ff3648567137d59
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-02-28 15:22:30 -0500

    added more logs

--- build/my-app/src/main/java/com/mycompany/app/model/GameModel.java
+++ build/my-app/src/main/java/com/mycompany/app/model/GameModel.java
@@ -42,7 +42,7 @@ public class GameModel{
 	protected void AI_Move(AbstractAI ai){
 		switch(this.state){
 			case BEGIN_TURN:
-				log.gameStateAction(this.state.toString(),"AI Draw Story",ai);
+				log.gameStateAction(this.state,"Draw Story",ai);
 
 				/* this can be a user interaction */
 				drawStoryCard();
@@ -50,35 +50,35 @@ public class GameModel{
 			/* Quest Setup */
 			// decide whether sponsor a quest
 			case SPONSOR_QUEST:
-				log.gameStateAction(this.state.toString(),"AI Sponsor Quest",ai);
+				log.gameStateAction(this.state,"Sponsor Quest",ai);
 				sponsorQuest(ai.id(),ai.doISponsorAQuest(this.board));
 				break;
 			// decide the quest setup
 			case SPONSOR_SUBMIT:
-				log.gameStateAction(this.state.toString(),"AI Submitting Sponsor",ai);
+				log.gameStateAction(this.state,"Submitting Sponsor",ai);
 				submitQuest(ai.id(),ai.sponsorQuest(this.board));
 				break;
 			// decide to participate in a quest
 			case PARTICIPATE_QUEST:
-				log.gameStateAction(this.state.toString(),"AI Participate Quest",ai);
+				log.gameStateAction(this.state,"Participate Quest",ai);
 				participateQuest(ai.id(),ai.doIParticipateInQuest(this.board));
 				break;
 			// continue onto next stage : is AI needed
 			case QUEST_HANDLER:
-				log.gameStateAction(this.state.toString(),"AI Begin Quest Stage",ai);
+				log.gameStateAction(this.state,"Begin Quest Stage",ai);
 
 				/* This can be a user interaction */
 				stage();
 				break;
 			// submit foes for a stage
 			case STAGE_FOE:
-				log.gameStateAction(this.state.toString(),"AI Move Foe",ai);
+				log.gameStateAction(this.state,"Move Foe",ai);
 				stageFoe(ai.id(),ai.playQuest(this.board));
 				break;
 			// submit bids for a stage
 			// decide whether to give up
 			case STAGE_TEST:
-				log.gameStateAction(this.state.toString(),"AI Bidding",ai);
+				log.gameStateAction(this.state,"Bidding",ai);
 				
 				// if the AI cannot id more, then give up
 				if(!stageTest(ai.id(),ai.nextBid(this.board))){
@@ -87,52 +87,52 @@ public class GameModel{
 				break;
 			// flip up cards : is AI needed
 			case STAGE_END:
-				log.gameStateAction(this.state.toString(),"AI Quest Stage End",ai);
+				log.gameStateAction(this.state,"Quest Stage End",ai);
 
 				/* Can be a user interaction */
 				stageEnd();
 				break;
 			// clear up quest : is AI needed here
 			case QUEST_END:
-				log.gameStateAction(this.state.toString(),"AI Quest End",ai);
+				log.gameStateAction(this.state,"Quest End",ai);
 				
 				/* Can be a user interaction */
 				endQuest();
 				break;
 			case PARTICIPATE_TOURNAMENT:
-				log.gameStateAction(this.state.toString(),"AI Participate Tournament",ai);
+				log.gameStateAction(this.state,"Participate Tournament",ai);
 				participateTournament(ai.id(),ai.doIParticipateInTournament(this.board));
 				break;
 			case TOURNAMENT_HANDLER:
-				log.gameStateAction(this.state.toString(),"AI Stage Begin",ai);
+				log.gameStateAction(this.state,"Stage Begin",ai);
 
 				/* Can be a user interaction */
 				tournamentStageStart();
 				break;
 			case TOURNAMENT_STAGE:
-				log.gameStateAction(this.state.toString(),"Play Tournament Stage",ai);
+				log.gameStateAction(this.state,"Play Tournament Stage",ai);
 
 				tournamentStage(ai.id(),ai.playInTournament(this.board));
 				break;
 			case TOURNAMENT_STAGE_END:
-				log.gameStateAction(this.state.toString(),"End Tournament Stage",ai);
+				log.gameStateAction(this.state,"End Tournament Stage",ai);
 
 				/* Can be a user interaction */
 				tournamentStageEnd();
 				break;
 			case TOURNAMENT_END:
-				log.gameStateAction(this.state.toString(),"End Tournament",ai);
+				log.gameStateAction(this.state,"End Tournament",ai);
 
 				/* Can be a user interaction */
 				tournamentEnd();
 			case EVENT_LOGIC:
-				log.gameStateAction(this.state.toString(),"Apply Event",ai);
+				log.gameStateAction(this.state,"Apply Event",ai);
 
 				/* Can be a user interaction */
 				applyEventLogic();
 			// end turn : is AI needed here
 			case END_TURN:
-				log.gameStateAction(this.state.toString(),"End Turn",ai);
+				log.gameStateAction(this.state,"End Turn",ai);
 
 				/* Can be a user interaction */
 				endTurn();
@@ -141,16 +141,22 @@ public class GameModel{
 	}
 
 	public boolean discard (int playerId, List<Card> discards){
-		if(this.state != GameStates.DISCARD)
+		if(this.state != GameStates.DISCARD){
+			log.gameStateAction(this.state,"Incorrect State",board.findPlayer(playerId));
 			return false;
+		}
 
-		if(this.discard.current() != playerId)
+		if(this.discard.current() != playerId){
+			log.gameStateAction(this.state,"Incorrect Player",board.findPlayer(playerId));
 			return false;
+		}
 
 		boolean valid = board.discardHand(playerId,discards);
 
-		if(!valid)
+		if(!valid){
+			log.gameStateAction(this.state,"Invalid Discard",board.findPlayer(playerId));
 			return false;
+		}
 
 		discard.removeCurrent();
 
@@ -169,8 +175,7 @@ public class GameModel{
 		log.gameState(state.toString());
 		// Discard
 		if(playersOverLimit.size() > 0 && this.state != GameStates.DISCARD){
-			log.gameStateAction(this.state.toString(),"Discarding",playerId);
-
+			log.gameStateAction(this.state,"Discard Found",board.findPlayer(playerId));
 			this.savedState = state;
 			this.savedIndex = this.players.indexOf(playerId);
 			this.state = GameStates.DISCARD;
@@ -366,13 +371,16 @@ public class GameModel{
 	 * NEEDS : some kind of quest object to submit
 	 */
 	public boolean submitQuest(int player,TwoDimensionalArrayList<Card> quest){
-		if(this.state != GameStates.SPONSOR_SUBMIT)
+		if(this.state != GameStates.SPONSOR_SUBMIT){
+			log.gameStateAction(this.state,"Incorrect State",board.findPlayer(player));
 			return false;
+		}
 
 		/*
 		 * verify that it is a valid quest
 		 */
 		if(questSponsor.current() == player && board.submitQuest(quest,player)){
+			log.gameStateAction(this.state,"Valid Submission",board.findPlayer(player));
 			this.participants = new Cycle(players,players.indexOf(questSponsor.current()));
 			this.participants.removeCurrent();
       
@@ -380,6 +388,7 @@ public class GameModel{
 			return true;
 			//this.state = GameStates.PARTICIPATE_QUEST;
 		}
+		log.gameStateAction(this.state,"Invalid Submission",board.findPlayer(player));
 		return false;
 
 	}
@@ -388,8 +397,10 @@ public class GameModel{
 	 * NEEDS : change player parameter to a Player Object
 	 */
 	public void participateQuest(int player,boolean participate){
-		if(this.state != GameStates.PARTICIPATE_QUEST)			
+		if(this.state != GameStates.PARTICIPATE_QUEST){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(player));
 			return;
+		}
 
 
 		/*
@@ -397,7 +408,7 @@ public class GameModel{
 		 */
 		int currPlayer = this.participants.current();
 		Player p = board.findPlayer(currPlayer);
-        log.playerAction(p, "is deciding whether to participate in the Quest");
+		log.playerAction(p, "is deciding whether to participate in the Quest");
 
 		if(player == currPlayer && participate){
 			log.playerAction(p, "successfully participates in the Quest");
@@ -411,22 +422,27 @@ public class GameModel{
 		// change state
 		if(this.participants.size() <= 0 && this.board.getParticipants().size()>0){
 			//this.state = GameStates.QUEST_HANDLER;
+			log.gameStateAction(this.state,"Starting Quest",board.findPlayer(player));
 			changeState(GameStates.QUEST_HANDLER, currPlayer);
 		}
 		else if(this.participants.size() <= 0 && this.board.getParticipants().size() <= 0){
 			//this.state = GameStates.QUEST_END;	
+			log.gameStateAction(this.state,"Not Enough Players",board.findPlayer(player));
 			changeState(GameStates.QUEST_END, currPlayer);
 		}
 		else if(this.state != GameStates.QUEST_HANDLER) {
-		    changeState(GameStates.PARTICIPATE_QUEST, this.participants.current());
+			log.gameStateAction(this.state,"Next Player",board.findPlayer(player));
+			changeState(GameStates.PARTICIPATE_QUEST, this.participants.current());
         }
 
 		this.updateObservers();
 	}
 
 	public void stage(){
-		if(this.state != GameStates.QUEST_HANDLER)
+		if(this.state != GameStates.QUEST_HANDLER){
+			log.gameStateAction(this.state,"Invalid State","");
 			return ;
+		}
 		/*
 		 * some kind of quest logic here
 		 */
@@ -438,10 +454,12 @@ public class GameModel{
 
 		if(board.stageType(Card.Types.FOE)){
 			//this.state = GameStates.STAGE_FOE;
+			log.gameStateAction(this.state,"Foe Stage Next","");
 			changeState(GameStates.STAGE_FOE,this.participants.current());
 		}
 		if(board.stageType(Card.Types.TEST)){
 			//this.state = GameStates.STAGE_TEST;
+			log.gameStateAction(this.state,"Test Stage Next","");
 			changeState(GameStates.STAGE_TEST,this.participants.current());
 		}
 
@@ -449,22 +467,30 @@ public class GameModel{
 	}
 
 	public boolean stageFoe(int playerID, List<Card> list){
-		if(this.state != GameStates.STAGE_FOE)
+		if(this.state != GameStates.STAGE_FOE){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(playerID));
 			return false;
+		}
 
-		if(playerID != this.participants.current())	
+		if(playerID != this.participants.current()){
+			log.gameStateAction(this.state,"Invalid Player",board.findPlayer(playerID));
 			return false;
+		}
+
 
 		boolean validSubmit = board.submitHand(playerID,list);
 
 		if(validSubmit){
+			log.gameStateAction(this.state,"valid Submission",board.findPlayer(playerID));
 			this.participants.removeCurrent();
 		}
 		if(!validSubmit){
+			log.gameStateAction(this.state,"invalid Submission",board.findPlayer(playerID));
 			return false;
 		}
 
 		if(this.participants.size() <= 0 ){
+			log.gameStateAction(this.state,"last participant",board.findPlayer(playerID));
 			changeState(GameStates.STAGE_END,playerID);
 			//this.state = GameStates.STAGE_END;
 		}
@@ -476,24 +502,31 @@ public class GameModel{
 	}
 
 	public boolean stageTest(int playerID, List<Card> list){
-		if(this.state != GameStates.STAGE_TEST)
+		if(this.state != GameStates.STAGE_TEST){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(playerID));
 			return false;
+		}
 
-		if(playerID != this.participants.current())
+		if(playerID != this.participants.current()){
+			log.gameStateAction(this.state,"Invalid Player",board.findPlayer(playerID));
 			return false;
+		}
 
 		boolean validSubmit = board.submitBids(playerID,list);
 
 		if(!validSubmit) {
+			log.gameStateAction(this.state,"Invalid Bid",board.findPlayer(playerID));
 			return false;
 		}
 
 		if(validSubmit && board.checkTestWinner()){
 			//this.state = GameStates.STAGE_END;
+			log.gameStateAction(this.state,"Wins Bid",board.findPlayer(playerID));
 			changeState(GameStates.STAGE_END,this.participants.current());
 		}
 
 		if(validSubmit && !board.checkTestWinner()){
+			log.gameStateAction(this.state,"Passes Bid",board.findPlayer(playerID));
 			this.participants.next();
 		}
 
@@ -504,16 +537,22 @@ public class GameModel{
 	}
 
 	public void testGiveUp(Integer id){
-		if(this.state != GameStates.STAGE_TEST)
-			return;
-
-		if(participants.current() != id){
+		if(this.state != GameStates.STAGE_TEST){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(id));
+			this.participants.next();
 			return;
 		}
 
+		if(participants.current() != id){
+			log.gameStateAction(this.state,"Invalid Player",board.findPlayer(id));
+			return;
+		}
+
+		log.gameStateAction(this.state,"Given Up",board.findPlayer(id));
 		board.giveUp(id);
 
 		if(board.checkTestWinner()){
+			log.gameStateAction(this.state,"Someone Has Won",board.findPlayer(id));
 			//this.state = GameStates.STAGE_END;
 			changeState(GameStates.STAGE_END,this.participants.current());
 		}
@@ -522,36 +561,44 @@ public class GameModel{
 
 
 	public void stageEnd(){
-		if(this.state != GameStates.STAGE_END)
+		if(this.state != GameStates.STAGE_END){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
 
 		if(board.stageType(Card.Types.FOE)){
+			log.gameStateAction(this.state,"Cleaning Up Foe Stage","");
 			board.completeFoeStage();
 		}
 		if(board.stageType(Card.Types.TEST)){
+			log.gameStateAction(this.state,"Cleaning Up Test Stage","");
 
 		}
 
 		if(board.getParticipants().size() == 0){
-			//this.state = GameStates.QUEST_END;
+			log.gameStateAction(this.state,"No More Participants","");
 			changeState(GameStates.QUEST_END,this.questSponsor.current());
 		}
 
 		//distribute cards
-		if(!this.board.nextStage())
+		if(!this.board.nextStage()){
+			log.gameStateAction(this.state,"Completed All Stages","");
 			changeState(GameStates.QUEST_END,this.questSponsor.current());
-			//this.state = GameStates.QUEST_END;
-		else
+		}
+		else{
+			log.gameStateAction(this.state,"Next Stage","");
 			changeState(GameStates.QUEST_HANDLER,this.questSponsor.current());
-			//this.state = GameStates.QUEST_HANDLER;
+		}
 		this.updateObservers();
 	}
 
 
 	public void endQuest() {
 		if(this.state != GameStates.QUEST_END) {
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
 		}
+		log.gameStateAction(this.state,"Applying Quest Logic","");
 
 		//apply story logic
 		board.applyStoryCardLogic(questSponsor.current());
@@ -563,8 +610,12 @@ public class GameModel{
 
 
 	public void beginTournament(){
-		if(this.state != GameStates.TOURNAMENT_HANDLER)
+		if(this.state != GameStates.TOURNAMENT_HANDLER){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
+
+		log.gameStateAction(this.state,"Begin Tournament","");
 
 		// start a cycle which loops through participants
 		participants = new Cycle<Integer>(players,players.indexOf(storyTurn.current()));
@@ -579,23 +630,30 @@ public class GameModel{
 	 * NEEDS : change player parameter to a Player Object
 	 */
 	public void participateTournament(int player, boolean participate){
-		if(this.state != GameStates.PARTICIPATE_TOURNAMENT)
+		if(this.state != GameStates.PARTICIPATE_TOURNAMENT){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(player));
 			return;
+		}
 		if(player == participants.current() && participate){
+			log.gameStateAction(this.state,"Participates",board.findPlayer(player));
 			this.board.addParticipant(this.participants.removeCurrent());
 		}
 		if(player == this.participants.current() && !participate){
-			this.board.addParticipant(this.participants.removeCurrent());
+			log.gameStateAction(this.state,"Does Not Participate",board.findPlayer(player));
+			this.participants.removeCurrent();
 		}
 
 		if(this.participants.size() <= 0 && board.getParticipants().size() > 1){
+			log.gameStateAction(this.state,"Beginning Tournament",board.findPlayer(player));
 			changeState(GameStates.TOURNAMENT_HANDLER,player);
 		}
 		else if(this.participants.size() <= 0 && board.getParticipants().size() <= 1){
+			log.gameStateAction(this.state,"Not Enough Participants",board.findPlayer(player));
 			changeState(GameStates.END_TURN,this.storyTurn.current());
 			//this.state = GameStates.END_TURN;
 		}
 		else{
+			log.gameStateAction(this.state,"Next Participant",board.findPlayer(player));
 			changeState(GameStates.PARTICIPATE_TOURNAMENT,this.participants.current());
 		}
 
@@ -604,9 +662,12 @@ public class GameModel{
 	}
 
 	public void tournamentStageStart(){
-		if(this.state != GameStates.TOURNAMENT_HANDLER)
+		if(this.state != GameStates.TOURNAMENT_HANDLER){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
 
+		log.gameStateAction(this.state,"Beginning Tournament Stage","");
 		/*
 		 * some kind of quest logic here
 		 */
@@ -623,25 +684,32 @@ public class GameModel{
 	}
 
 	public boolean tournamentStage(int id,List<Card> hand){
-		if(this.state != GameStates.TOURNAMENT_STAGE)
+		if(this.state != GameStates.TOURNAMENT_STAGE){
+			log.gameStateAction(this.state,"Invalid State",board.findPlayer(id));
 			return false;
+		}
 
-		if(id != this.participants.current())
+		if(id != this.participants.current()){
+			log.gameStateAction(this.state,"Invalid Player",board.findPlayer(id));
 			return false;
+		}
 
 		// implement the tournament methods
 		boolean validSubmit = board.submitHand(id,hand);
 
 		if(!validSubmit){
+			log.gameStateAction(this.state,"Incorrect Submission",board.findPlayer(id));
 			return false;
 		}
 
 		if(validSubmit){
+			log.gameStateAction(this.state,"Correct Submission",board.findPlayer(id));
 			this.participants.removeCurrent();
 		}
 
 		
 		if(this.participants.size() <= 0){
+			log.gameStateAction(this.state,"Ending Stage",board.findPlayer(id));
 			changeState(GameStates.TOURNAMENT_STAGE_END,id);
 			//this.state = GameStates.TOURNAMENT_STAGE_END;
 		}
@@ -650,9 +718,12 @@ public class GameModel{
 	}
 
 	public void tournamentStageEnd(){
-		if(this.state != GameStates.TOURNAMENT_STAGE_END)
+		if(this.state != GameStates.TOURNAMENT_STAGE_END){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
-		
+		}
+
+
 		//check for winner
 		board.completeTournamentStage();
 
@@ -662,11 +733,13 @@ public class GameModel{
 		if(anotherRound){
 			//clean up round 1, 
 			//this.state = GameStates.TOURNAMENT_HANDLER;
+			log.gameStateAction(this.state,"Tie Breaker","");
 			changeState(GameStates.TOURNAMENT_HANDLER,this.participants.current());
 		}
 		//TIE round 2 
 		if(!anotherRound){
 			//clean up all	
+			log.gameStateAction(this.state,"Winner Found","");
 			changeState(GameStates.TOURNAMENT_END,this.storyTurn.current());
 			//this.state = GameStates.TOURNAMENT_END;
 		}
@@ -675,10 +748,13 @@ public class GameModel{
 	}
 
 	public void tournamentEnd(){
-		if(this.state != GameStates.TOURNAMENT_END)
+		if(this.state != GameStates.TOURNAMENT_END){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
 
 
+		log.gameStateAction(this.state,"Applying Tournament Logic","");
 		board.applyStoryCardLogic(-1);
 		changeState(GameStates.END_TURN,this.storyTurn.current());
 		//this.state = GameStates.END_TURN;
@@ -688,12 +764,15 @@ public class GameModel{
 
 
 	public void applyEventLogic(){
-		if(this.state != GameStates.EVENT_LOGIC)
+		if(this.state != GameStates.EVENT_LOGIC){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
 
 		/*
 		 * ACTION : Apply events logic to players
 		 */
+		log.gameStateAction(this.state,"Applying Event Logic","");
 		board.applyStoryCardLogic(storyTurn.current());
 
 		//this.state = GameStates.END_TURN;
@@ -704,9 +783,12 @@ public class GameModel{
 
 
 	public void endTurn(){
-		if(this.state != GameStates.END_TURN)
+		if(this.state != GameStates.END_TURN){
+			log.gameStateAction(this.state,"Invalid State","");
 			return;
+		}
 		
+		log.gameStateAction(this.state,"End Turn","");
 		this.turn++;
 		this.currentPlayer = storyTurn.next();
 
