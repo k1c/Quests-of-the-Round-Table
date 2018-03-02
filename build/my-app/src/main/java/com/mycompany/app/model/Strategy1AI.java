3864041389ad62f850a2beef334de3618a6b01e2
k1c <cpelletier@klipfolio.com>
2018-03-02 13:07:42 -0500

    hotfix

--- build/my-app/src/main/java/com/mycompany/app/model/Strategy1AI.java
+++ build/my-app/src/main/java/com/mycompany/app/model/Strategy1AI.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class Strategy1AI extends AbstractAI {
 
-    public Strategy1AI(){
+    public Strategy1AI(String name, String shieldImage){
+        super(name,shieldImage);
         this.behaviour = new Strategy1();
     }
 
