e5ca81a6a59372dc1f2295dfb10db38489624926
d4sh <akhil.d@hotmail.com>
2018-04-07 13:35:49 -0400

    create/join/lobby pages

--- build/my-app/src/main/java/com/mycompany/app/controller/GreetingController.java
+++ build/my-app/src/main/java/com/mycompany/app/controller/GreetingController.java
@@ -6,11 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.mycompany.app.model.GameModel;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class GreetingController {
 
     private static GameModel gameModel = new GameModel();
-
+    private int i = 0;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -18,8 +21,18 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping("/newgame")
+    public String newgame() {
+        return "newgame";
+    }
+
+    @GetMapping("/lobby")
+    public String lobby(HttpServletResponse response) {
+        response.addCookie(new Cookie("id", "" + i++));
+        return "lobby";
+    }
     @ResponseBody
-    @PostMapping("/setup")
+    @PostMapping("/creategame")
     public void setup(){
         String[] s = {"Akhil", "Carolyne", "Meow"};
         gameModel.initGame(3, 0, 0, s);
