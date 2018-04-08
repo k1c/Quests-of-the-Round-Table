238aaad04c3e8f14aa74f87f7105d964f2845304
k1c <cpelletier@klipfolio.com>
2018-04-07 22:41:32 -0400

    Strategy1 doIParticipateInTournament

--- build/my-app/src/main/java/com/mycompany/app/model/Players/Strategy1.java
+++ build/my-app/src/main/java/com/mycompany/app/model/Players/Strategy1.java
@@ -9,9 +9,38 @@ public class Strategy1 extends AbstractStrategyBehaviour{
      * Return Type : TRUE -- I want to participate
      * 		 FALSE - I do not participate
      */
+
+    protected List<AdventureCard> allPossibleCards(GameBoard board, AbstractAI ai){
+
+        Set<AdventureCard> set = new TreeSet(); List<AdventureCard> temp = new ArrayList();
+
+        boolean amourInPlay = false;
+
+        amourInPlay = board.cardListHas(ai.inPlay,Card.Types.AMOUR);
+
+        for(AdventureCard item : ai.hand){
+            if (item.type == Card.Types.ALLY){
+                set.add(item);
+            }
+            if (item.type == Card.Types.WEAPON){
+                set.add(item);
+            }
+            if(item.type == Card.Types.AMOUR && !amourInPlay){
+                set.add(item);
+                amourInPlay = true;
+            }
+
+            continue;
+        }
+
+        temp.addAll(set);
+        return temp;
+    }
+
+
     public boolean doIParticipateInTournament(GameBoard board, AbstractAI ai){
 
-        List<Player> players = board.getParticipantPlayers();
+        List<Player> players = board.players;
         int currentRank = 0;
         boolean win = false;
 
@@ -19,11 +48,8 @@ public class Strategy1 extends AbstractStrategyBehaviour{
 
         for (Player p : players ) {
             if (p.rank.getShields() + (board.getCurrentQuestStages() - players.size()) >= p.rank.getMaxShields()) {
-                win = true;
+                return true;
             }
-        }
-        if (win == true){
-            return true;
         }
 
         return false;
@@ -35,13 +61,52 @@ public class Strategy1 extends AbstractStrategyBehaviour{
      * Return Type : List<Card>
      */
     public List<Card> playInTournament(GameBoard board, AbstractAI ai){
-        return new ArrayList();
+
+        List<Player> players = board.getParticipantPlayers();
+        int currentRank = 0;
+        boolean win = false;
+
+        ViewGameBoard b = board.getViewCopy();
+
+        for (Player p : players ) {
+            if (p.rank.getShields() + (board.getCurrentQuestStages() - players.size()) >= p.rank.getMaxShields()) {
+                return allPossibleCards(board,ai);
+            }
+        }
+
+        List<Card> aiTournament = new List<Card>();
+        List<AdventureCard> tempAIHand = ai.hand;
+        List<AdventureCard> tempWeapons = new ArrayList<AdventureCard>();
+
+        for(int i = 0; i < tempAIHand.size(); i++){
+            if (tempAIHand.get(i).type == Card.Types.WEAPON){
+                tempWeapons.add(tempAIHand.get(i));
+            }
+        }
+
+        Map<Card, Integer> cardDuplicates = new HashMap<Card, Integer>();
+        for (int i = 0; i < tempWeapons.size(); i++) {
+            if (cardDuplicates.get(tempWeapons.get(i)) != null) {
+                cardDuplicates.put(tempWeapons.get(i), cardDuplicates.get(tempWeapons.get(i)) + 1);
+            } else {
+                cardDuplicates.put(tempWeapons.get(i), 1);
+            }
+        }
+
+        for (Map.Entry<Card, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                aiTournament.add(entry.getKey());
+            }
+        }
+
+        return aiTournament;
     }
 
     /* Description : Returns whether AI will sponsor a quest or not
      * Return Type : TRUE -- I want to participate
      * 		 FALSE - I do not participate
      */
+
     public boolean doISponsorAQuest(GameBoard board, AbstractAI ai){
         return false;
     }
