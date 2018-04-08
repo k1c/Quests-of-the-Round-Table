12eb4e0d50da63ebbd296214763a21af76456fb1
k1c <cpelletier@klipfolio.com>
2018-04-07 22:59:47 -0400

    Strategy1 doIParticipateInQuest and doISponsorAQuest

--- build/my-app/src/main/java/com/mycompany/app/model/Players/Strategy1.java
+++ build/my-app/src/main/java/com/mycompany/app/model/Players/Strategy1.java
@@ -108,6 +108,52 @@ public class Strategy1 extends AbstractStrategyBehaviour{
      */
 
     public boolean doISponsorAQuest(GameBoard board, AbstractAI ai){
+
+        List<Player> players = board.getParticipantPlayers();
+
+        for (Player p : players) {
+            if (p.id != ai.id){
+                if (p.rank.getShields() + (board.getCurrentQuestStages() - players.size()) >= p.rank.getMaxShields()) {
+                    return false;
+                }
+            }
+        }
+        if(canISetup1 == false){
+            return false;
+        }
+
+        if (board.playerCanSponsor(ai.id)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    private boolean canISetup1(GameBoard board, AbstractAI ai){
+
+        ArrayList<Card> foeList = new ArrayList<Card>();
+        ArrayList<Card> weaponList = new ArrayList<Card>();
+
+        for(int i = 0; i < ai.hand.size(); i++){
+            if(ai.hand.get(i).type == Card.Types.FOE){
+                foeList.add(ai.hand.get(i));
+            }
+            if(ai.hand.get(i).type == Card.Types.WEAPON){
+                weaponList.add(ai.hand.get(i));
+            }
+        }
+
+        //set up last stage to be at least 40
+        int questStageBP = 0;
+        questStageBP += foeList.get(foeList.size() - 1).getBattlePoints();
+        if(questStageBP < 50) {
+            for (int j = weaponList.size() - 1; j > 0; j--) {
+                questStageBP += weaponList.get(j).getBattlePoints();
+                if (questStageBP >= 50) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -116,7 +162,60 @@ public class Strategy1 extends AbstractStrategyBehaviour{
      * Return Type : TwoDimensionalArrayList<Card>
      */
     public TwoDimensionalArrayList<Card> sponsorQuest(GameBoard board, AbstractAI ai){
-        return new TwoDimensionalArrayList();
+
+        TwoDimensionalArrayList<Card> aiQuest = new TwoDimensionalArrayList<Card>();
+        ArrayList<Card> foeList = new ArrayList<Card>();
+        ArrayList<Card> testList = new ArrayList<Card>();
+        ArrayList<Card> weaponList = new ArrayList<Card>();
+
+        for(int i = 0; i < ai.hand.size(); i++){
+            if(ai.hand.get(i).type == Card.Types.FOE){
+                foeList.add(ai.hand.get(i));
+            }
+            if(ai.hand.get(i).type == Card.Types.TEST){
+                testList.add(ai.hand.get(i));
+            }
+            if(ai.hand.get(i).type == Card.Types.WEAPON){
+                weaponList.add(ai.hand.get(i));
+            }
+        }
+
+        //set up last stage to be at least 50
+        int questStageBP = 0;
+        ArrayList<Card> stageN = new ArrayList<Card>();
+        questStageBP += foeList.get(foeList.size() - 1).getBattlePoints();
+        stageN.add(foeList.get(foeList.size() - 1));
+        foeList.remove(foeList.get(foeList.size() - 1));
+        if(questStageBP < 50) {
+            for (int j = weaponList.size() - 1; j > 0; j--) {
+                questStageBP += weaponList.get(j).getBattlePoints();
+                stageN.add(weaponList.get(j));
+                if (questStageBP >= 50) {
+                    break;
+                }
+            }
+        }
+        aiQuest.add(board.currentStory.getNumStages()-1, stageN);
+
+        if(testList.size() > 0) {
+            ArrayList<Card> testStage = new ArrayList<Card>();
+            testStage.add(testList.get(0));
+            aiQuest.add(currentStory.getNumStages() - 2, testStage);
+            for (int i = 0; i < currentStory.getNumStages() - 2; i++) {
+                ArrayList<Card> temp = new ArrayList<Card>();
+                temp.add(foeList.get(i));
+                aiQuest.add(i, temp);
+            }
+        } else {
+            //TODO: change this
+            /*for (int i = 0; i < currentStory.getNumStages() - 1; i++) {
+                ArrayList<Card> temp = new ArrayList<Card>();
+                temp.add(foeList.get(i));
+                aiQuest.add(i, temp);*/
+            }
+        }
+
+        return aiQuest;
     }
 
 
@@ -125,6 +224,37 @@ public class Strategy1 extends AbstractStrategyBehaviour{
      * 		 FALSE - I do not participate
      */
     public boolean doIParticipateInQuest(GameBoard board, AbstractAI ai){
+
+        ArrayList<AdventureCard> allyList = new ArrayList<AdventureCard>();
+        ArrayList<AdventureCard> weaponList = new ArrayList<AdventureCard>();
+
+        //C1
+        for (int i = 0; i < ai.hand.size(); i++) {
+            if (allPos.get(i).type == Card.Types.ALLY) {
+                allyList.add(ai.hand.get(i));
+            }
+            if (allPos.get(i).type == Card.Types.WEAPON) {
+                weaponList.add(ai.hand.get(i));
+            }
+        }
+
+        if((allyList.size() + weaponList.size()) >= 2 * board.getCurrentQuestStages()) {
+            //C2
+
+            int foeCounter = 0;
+
+            for(int i = 0; i < ai.hand.size(); i++){
+                if (ai.hand.get(i).type == Card.Types.FOE){
+                    if(ai.hand.get(i).getBattlePoints() < 20){
+                        foeCounter++;
+                        if(foeCounter == 2){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
         return false;
     }
 
