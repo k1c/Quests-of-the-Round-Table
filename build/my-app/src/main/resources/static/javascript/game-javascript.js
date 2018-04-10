0a604c9808e8322084406d34df883d0c138e6fc9
Akhil Dalal <akhil.d@hotmail.com>
2018-04-10 16:52:41 -0400

    refactoring the js code

--- build/my-app/src/main/resources/static/javascript/game-javascript.js
+++ build/my-app/src/main/resources/static/javascript/game-javascript.js
@@ -6,40 +6,11 @@ On game load, display:
 4) console
  */
 $(document).ready(function() {
-    $.ajax({
-        method:"GET",
-        url: "/currentplayer",
-        data: {id: parseInt(document.cookie.split("=")[1])},
-        success: function(player) {
-            $("tr", '#decks').append('<td><img src="images/A King Arthur.jpg"/></td>')
-            $("tr", '#decks').append('<td><img src="images/S Back.jpg"/></td>')
-            var WIDTH = 200;
-            var X_OFFSET = WIDTH/3;
+    $("tr", '#decks').append('<td><img src="images/A Back.jpg"/></td>');
+    $("tr", '#decks').append('<td><img src="images/S Back.jpg"/></td>');
 
-            var hand = player.hand;
-            var inplay = player.inPlay;
-            var cardsPerCol = WIDTH / X_OFFSET;
-            var handSpan = Math.floor(hand.length/cardsPerCol + 1);
-            var inplaySpan = Math.ceil(inplay.length/cardsPerCol + 1);
-
-            var numCol = 1 + 1 + handSpan + 1 + inplaySpan + 1 + 1 + 1;
-
-            for (var i = numCol - 1; i >= 0; i--) {
-                $("tr", "#current-player").append("<td data-id='" + i +"'></td>");
-            }
-
-            $('[data-id="0"]').append('<img src="images/' + player.rank.path +'" />');
-            $('[data-id="1"]').append('<img style="height: 80%; top:0;" src="images/' + player.shieldImage +'" />');
-            var cell = $('[data-id="2"]');
-            cell.attr("colspan", handSpan);
-
-            for (i = 0; i < hand.length; i++) {
-               cell.append('<img data-type="hand" data-id="' + i + '" onmouseover="focusCard(this)" onmouseleave="unfocusCard(this)" style="transform: translateX(' + -i*50 + 'px);" src="images/' + hand[i].res +'" />');
-            }
-
-            waiting_players();
-        }
-    });
+    current_player();
+    waiting_players();
 });
 
 var x = 99;
@@ -77,11 +48,51 @@ function unfocusCard(c) {
 function getState() {
 
 }
+
 /* ---------- */
 /* COMPONENTS */
 /* ---------- */
 
 /* Current Player */
+function current_player() {
+    $.ajax({
+        method:"GET",
+        url: "/currentplayer",
+        data: {id: parseInt(document.cookie.split("=")[1])},
+        success: function(player) {
+            var WIDTH = 200;
+            var X_OFFSET = WIDTH/3;
+
+            var hand = player.hand;
+            var inplay = player.inPlay;
+            var cardsPerCol = WIDTH / X_OFFSET;
+            var handSpan = Math.floor(hand.length/cardsPerCol + 1);
+            var inplaySpan = Math.ceil(inplay.length/cardsPerCol + 1);
+
+            var numCol = 1 + 1 + handSpan + 1 + inplaySpan + 1 + 1 + 1;
+
+            for (var i = numCol - 1; i >= 0; i--) {
+                $("tr", "#current-player").append("<td data-id='" + i +"'></td>");
+            }
+
+            $('[data-id="0"]').append('<img src="images/' + player.rank.path +'" />');
+            $('[data-id="1"]').append('<img style="height: 80%; top:0;" src="images/' + player.shieldImage +'" />');
+            var cell = $('[data-id="2"]');
+
+            cell.attr("colspan", handSpan);
+
+            for (i = 0; i < hand.length; i++) {
+                cell.append('<img ' +
+                    'data-type="hand" ' +
+                    'data-id="' + i + '" ' +
+                    'onmouseover="focusCard(this)" ' +
+                    'onmouseleave="unfocusCard(this)" ' +
+                    'style="transform: translateX(' + -i*50 + 'px);" ' +
+                    'src="images/' + hand[i].res +'" />');
+            }
+        }
+    });
+}
 
 /* Other Players */
 function waiting_players() {
