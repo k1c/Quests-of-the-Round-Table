b2790b0851bc21d1abaed478265202a59388e2f8
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-04-13 01:03:07 -0400

    added two more options for starting the game

--- build/my-app/src/main/java/com/mycompany/app/model/GameStateInit.java
+++ build/my-app/src/main/java/com/mycompany/app/model/GameStateInit.java
@@ -28,34 +28,4 @@ public class GameStateInit extends GameState{
 	public boolean quest(int playerId, TwoDimensionalArrayList<Card> quest){
 		return false;
 	}
-	public void newGame(int numHumans,int ai_type1,int ai_type2,String[] humanNames){
-
-		String[] shieldImages = {"Shield Blue.png", "Shield Red.png", "Shield Green.png", "Shield Purple.png"};
-		List<HumanPlayer> h = new ArrayList();
-		List<AbstractAI> a = new ArrayList();
-
-		model.turn = 0;
-
-		int num = Math.min(numHumans + ai_type1 + ai_type2, 4);
-
-		for(int i = 0; i < num; i++){
-			if(i < numHumans){
-				h.add(new HumanPlayer(humanNames[i],shieldImages[i]));			
-			}
-			else if(i < numHumans + ai_type1){
-				a.add(new Strategy1AI(String.format("AI%d", i+1-numHumans),shieldImages[i]));			
-			}
-			else if(i < numHumans + ai_type1 + ai_type2){
-				a.add(new Strategy1AI(String.format("AI%d", i+1-numHumans-ai_type1),shieldImages[i]));			
-			}
-		}
-
-		model.board.initRig(a,h,CardLoader.loadAdventureCards(),CardLoader.loadStoryCards(),true,true,true);
-			
-
-		model.players = model.board.getPlayerIds();		
-		model.storyTurn = new Cycle<Integer>(model.players,0);
-		
-		model.gameState = new GameStateTurn(this,model.storyTurn.current());
-	}
 }
